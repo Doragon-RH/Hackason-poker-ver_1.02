840d8c0b078e8a62069a6b6bd0d8bb35c4d5b4fb
Doragon-RH <r216af@icloud.com>
2024-06-13 13:25:36 +0900

    プレイヤー7,8を追加

--- src/services/players/index.tsx
+++ src/services/players/index.tsx
@@ -1,7 +1,8 @@
-import JsPlayer from './demo-player-js'; // 変更禁止
-import TsPlayer from './demo-player-ts'; // 変更禁止
+import JsPlayer from "./demo-player-js"; // 変更禁止
+import TsPlayer from "./demo-player-ts"; // 変更禁止
 // ↓作成したプレイヤープログラムをimportしてください。
-// import Team1 from './player'; // sample
+import Team7 from "./team7"; // sample
+import Team8 from "./team8"; // sample
 // ↑作成したプレイヤープログラムをimportしてください。
 
 const Players: {
@@ -11,7 +12,8 @@ const Players: {
   DemoPlayer2: TsPlayer, // 変更禁止
   DemoPlayer3: TsPlayer, // 変更禁止
   DemoPlayer4: JsPlayer, // 変更禁止
-
+  team_7: Team7,
+  team_8: Team8,
   // ↓作成したプレイヤープログラムを定義してください。
   // Team1, // sample
   // ↑作成したプレイヤープログラムを定義してください。
